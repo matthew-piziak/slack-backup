4cf041331063e5b337eebb5ba1a564d6d1538c43
Matthew Piziak <matthew.piziak@gmail.com>
2016-10-17 15:36:07 -0400

    read auth files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # slack-backup
  Personal backup utility for Slack
+
+ Experimental; not intended for secure usage.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 extern crate slack_api;
 extern crate hyper;
 
+use std::io::prelude::*;
+use std::io::Result;
+use std::fs::File;
+
 fn main() {
     let client = hyper::client::Client::new();
-    match slack_api::channels::list(&client, "TEST_TOKEN", None) {
+    let client_id = auth_file_contents("CLIENT_ID").unwrap();
+    let client_secret = auth_file_contents("CLIENT_SECRET").unwrap();
+    let access_response = slack_api::oauth::access(&client,
+                                                   &client_id,
+                                                   &client_secret,
+                                                   "CODE",
+                                                   None)
+                              .expect("OAuth access request failed");
+    match slack_api::channels::list(&client, &access_response.access_token, None) {
         Ok(list_response) => {
             for channel in list_response.channels {
                 println!("channel: {}", channel.name);
             }
         }
-        Err(err) => println!("{}", err)
+        Err(err) => println!("{}", err),
     }
+}
+
+fn auth_file_contents(filename: &str) -> Result<String> {
+    let mut file = File::open(&format!("../auth/{}", filename))
+                       .expect(&format!("{} file not found", filename));
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    return Ok(contents);
 }
